d6977b9518d91faa058ac5309b4d74b9226ae7b5
Murshed <murshed4400@gmail.com>
2021-10-30 17:58:06 +0530

    Welcome"

--- welcome.php
+++ welcome.php
@@ -2,3 +2,23 @@
     session_start();
     $id= $_SESSION['user'];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="container-fluid">
+        <div class="text-center justify-center">
+            <h1 class="">Welcome</h1>
+        </div>
+    </div>
+    
+</body>
+</html>
